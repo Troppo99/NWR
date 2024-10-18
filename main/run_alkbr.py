6d7829b42ca7100892e37e705aa31bcb397c4b12
Troppo99 <troppolungo99@gmail.com>
2024-10-18 09:16:08 +0700

    AWAL

--- main/run_alkbr.py
+++ main/run_alkbr.py
@@ -1,16 +1,19 @@
-from ALKBR import run_detection
+from ALKBR import display_thread, run_detection
 import threading
+import queue
 
 camera_names = ["10.5.0.161", "10.5.0.170", "10.5.0.182"]
+frame_queue = queue.Queue(maxsize=20)
+stop_flag = threading.Event()
 threads = []
-stop_flags = []
-
 for camera_name in camera_names:
-    stop_flag = threading.Event()
-    stop_flags.append(stop_flag)
-    thread = threading.Thread(target=run_detection, args=(camera_name, stop_flag))
+    thread = threading.Thread(target=run_detection, args=(camera_name, frame_queue, stop_flag))
     threads.append(thread)
     thread.start()
 
+display_thread_instance = threading.Thread(target=display_thread, args=(frame_queue, stop_flag))
+display_thread_instance.start()
+
 for thread in threads:
     thread.join()
+display_thread_instance.join()
