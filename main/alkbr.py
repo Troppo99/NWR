d59535a3335523edf7dc589bb6359dd0cb725e87
Troppo99 <troppolungo99@gmail.com>
2024-10-18 09:43:34 +0700

    update

--- main/alkbr.py
+++ main/alkbr.py
@@ -1,4 +1,5 @@
 import os
+
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 import cv2
 from ultralytics import YOLO
@@ -6,7 +7,6 @@ import numpy as np
 import time
 import queue
 import threading
-
 
 
 class opencvSection:
@@ -33,38 +33,26 @@ class opencvSection:
                 frame_queue.put((self.video, frame), block=False)
             except queue.Full:
                 pass
-
-        self.cap.release()  # Pastikan untuk melepaskan sumber daya kamera saat thread berhenti
+        self.cap.release()
 
 
 def run_detection(camera_name, frame_queue, stop_flag):
-    # Menghubungkan ke kamera dan memulai streaming
     video_path, _ = camera(camera_name)
     opencv = opencvSection(video_path)
     cap = opencv.connect_to_stream()
-
-    # Mengambil frame secara berulang dan memasukkannya ke queue
     opencv.read_frame(frame_queue, stop_flag)
 
 
 def display_frames(frame_queue, stop_flag, threads):
-    # Menampilkan frame di thread utama
     while not stop_flag.is_set():
         if not frame_queue.empty():
             camera_name, frame = frame_queue.get()
-            # Tampilkan frame pada window yang berbeda untuk setiap kamera
             cv2.imshow(f"ALKBR TESTING - {camera_name}", frame)
-
-            # Jika tombol 'n' ditekan, stop_flag diatur untuk menghentikan semua operasi
             if cv2.waitKey(1) & 0xFF == ord("n"):
                 stop_flag.set()
                 break
-
-    # Tunggu semua thread pengambil frame selesai sebelum keluar
     for thread in threads:
         thread.join()
-
-    # Tutup semua jendela OpenCV
     cv2.destroyAllWindows()
 
 
@@ -85,16 +73,14 @@ if __name__ == "__main__":
     stop_flag = threading.Event()
     threads = []
 
-    # Membuat thread untuk pengambilan frame setiap kamera
     for camera_name in camera_names:
         thread = threading.Thread(target=run_detection, args=(camera_name, frame_queue, stop_flag))
         threads.append(thread)
         thread.start()
 
-    # Mengelola display frame di thread utama
     try:
         display_frames(frame_queue, stop_flag, threads)
     finally:
-        stop_flag.set()  # Set stop flag agar semua thread berhenti
+        stop_flag.set()
         for thread in threads:
-            thread.join()  # Pastikan semua thread telah dihentikan dengan benar
+            thread.join()
