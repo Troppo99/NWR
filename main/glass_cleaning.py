d142146323dbfb93cddbf04dce3cc18e2f9293a1
Troppo99 <troppolungo99@gmail.com>
2024-10-22 13:32:14 +0700

    glass oke

--- main/glass_cleaning.py
+++ main/glass_cleaning.py
@@ -10,7 +10,45 @@ import time
 import numpy as np
 
 stop_flag = False
-borders = [[(48, 341), (77, 334), (70, 302), (41, 310)], [(77, 334), (108, 326), (101, 292), (70, 302)], [(108, 326), (141, 320), (133, 283), (101, 292)]]
+borders = [
+    [(30, 258), (60, 253), (58, 211), (24, 217)],
+    [(58, 211), (56, 169), (23, 173), (24, 217)],
+    [(23, 173), (56, 169), (57, 132), (20, 139)],
+    [(20, 139), (57, 132), (58, 78), (21, 97)],
+    [(69, 76), (102, 61), (99, 109), (68, 122)],
+    [(68, 122), (99, 109), (99, 142), (66, 161)],
+    [(66, 161), (99, 142), (99, 178), (67, 197)],
+    [(67, 197), (99, 178), (103, 219), (68, 249)],
+    [(114, 54), (148, 36), (146, 78), (113, 98)],
+    [(113, 98), (146, 78), (145, 117), (113, 133)],
+    [(113, 133), (145, 117), (141, 151), (112, 169)],
+    [(112, 169), (141, 151), (142, 191), (113, 214)],
+    [(160, 35), (183, 25), (181, 66), (158, 75)],
+    [(158, 75), (181, 66), (179, 98), (155, 111)],
+    [(155, 111), (179, 98), (178, 134), (153, 148)],
+    [(153, 148), (178, 134), (181, 165), (153, 184)],
+    [(197, 21), (219, 12), (214, 46), (192, 55)],
+    [(192, 55), (214, 46), (214, 83), (191, 93)],
+    [(191, 93), (214, 83), (213, 112), (191, 126)],
+    [(191, 126), (213, 112), (211, 143), (191, 160)],
+    [(230, 9), (247, 1), (245, 33), (226, 43)],
+    [(226, 43), (245, 33), (243, 69), (226, 77)],
+    [(226, 77), (243, 69), (244, 97), (225, 110)],
+    [(225, 110), (244, 97), (243, 122), (221, 137)],
+    [(275, 86), (285, 81), (286, 61), (274, 64)],
+    [(274, 64), (286, 61), (288, 1), (288, 1), (278, 2)],
+    [(285, 81), (308, 79), (309, 33), (292, 33)],
+    [(292, 33), (309, 33), (311, 4), (288, 1)],
+    [(308, 79), (328, 72), (331, 34), (309, 33)],
+    [(309, 33), (331, 34), (333, 2), (311, 4)],
+    [(328, 72), (350, 62), (350, 30), (331, 34)],
+    [(331, 34), (350, 30), (352, 2), (333, 2)],
+    [(350, 62), (369, 53), (369, 25), (350, 30)],
+    [(350, 30), (369, 25), (371, 1), (352, 2)],
+    [(369, 53), (387, 44), (385, 20), (369, 25)],
+    [(369, 25), (385, 20), (387, 3), (371, 1)],
+]
+print(len(borders))
 
 
 def process_model(frame):
@@ -34,6 +72,7 @@ def read_frames(cap, frame_queue):
 
 
 def draw_pose(frame, keypoint_coords, pairs, green_pairs, blue_pairs, pink_pairs, orange_pairs, scaled_borders_pts, border_states):
+    global keypoint_overlap
     for i, coord in enumerate(keypoint_coords):
         if coord:
             x, y = coord
@@ -53,9 +92,14 @@ def draw_pose(frame, keypoint_coords, pairs, green_pairs, blue_pairs, pink_pairs
                         x, y = x_new, y_new
                 radius = 30
                 point = (x, y)
+                overlap = False
                 for idx, border in enumerate(scaled_borders_pts):
-                    if cv2.pointPolygonTest(border, point, False) >= 0:
-                        border_states[idx] = True
+                    dist = cv2.pointPolygonTest(border, point, True)
+                    if dist >= -radius:
+                        border_states[idx]["is_green"] = True
+                        overlap = True
+                if overlap:
+                    keypoint_overlap = True  # Keypoints are overlapping with some border
                 cv2.circle(frame, (x, y), radius, (0, 255, 255), -1)
             elif i == 10:
                 kp8 = keypoint_coords[8]
@@ -73,9 +117,14 @@ def draw_pose(frame, keypoint_coords, pairs, green_pairs, blue_pairs, pink_pairs
                         x, y = x_new, y_new
                 radius = 30
                 point = (x, y)
+                overlap = False
                 for idx, border in enumerate(scaled_borders_pts):
-                    if cv2.pointPolygonTest(border, point, False) >= 0:
-                        border_states[idx] = True
+                    dist = cv2.pointPolygonTest(border, point, True)
+                    if dist >= -radius:
+                        border_states[idx]["is_green"] = True
+                        overlap = True
+                if overlap:
+                    keypoint_overlap = True  # Keypoints are overlapping with some border
                 cv2.circle(frame, (x, y), radius, (0, 255, 255), -1)
             else:
                 radius = 5
@@ -96,15 +145,18 @@ def draw_pose(frame, keypoint_coords, pairs, green_pairs, blue_pairs, pink_pairs
 
 
 def process_frames(frame_queue):
-    global stop_flag
+    global stop_flag, keypoint_overlap, keypoint_absence_timer_start
     pairs = [(0, 1), (0, 2), (1, 2), (2, 4), (1, 3), (4, 6), (3, 5), (5, 6), (6, 8), (8, 10), (5, 7), (7, 9), (6, 12), (12, 11), (11, 5), (12, 14), (14, 16), (11, 13), (13, 15)]
     green_pairs = {(0, 1), (0, 2), (1, 2), (1, 3), (2, 4), (3, 5), (4, 6)}
     blue_pairs = {(8, 10), (6, 8), (5, 6), (5, 7), (7, 9)}
     pink_pairs = {(6, 12), (11, 12), (5, 11)}
     orange_pairs = {(14, 16), (12, 14), (11, 13), (13, 15)}
 
-    border_states = [False] * len(borders)
+    # Initialize border states
+    border_states = [{"is_green": False} for _ in borders]
+    keypoint_absence_timer_start = None
     first_frame = True
+
     while not stop_flag:
         if not frame_queue.empty():
             frame = frame_queue.get()
@@ -125,6 +177,9 @@ def process_frames(frame_queue):
 
             results = process_model(frame)
 
+            current_time = time.time()
+            keypoint_overlap = False  # Reset overlap flag
+
             for result in results:
                 keypoints_data = result.keypoints.data
 
@@ -132,17 +187,31 @@ def process_frames(frame_queue):
                     keypoint_coords = [(int(x), int(y)) if confidence > 0.5 else None for x, y, confidence in keypoints]
                     draw_pose(frame, keypoint_coords, pairs, green_pairs, blue_pairs, pink_pairs, orange_pairs, scaled_borders_pts, border_states)
 
+            # Check for reset condition
+            if any(state["is_green"] for state in border_states):
+                if keypoint_overlap:
+                    keypoint_absence_timer_start = None
+                else:
+                    if keypoint_absence_timer_start is None:
+                        keypoint_absence_timer_start = current_time
+                    elif (current_time - keypoint_absence_timer_start) >= 3:
+                        # Reset all borders to yellow
+                        print("Resetting all borders to yellow after 3 seconds of no overlap.")
+                        for state in border_states:
+                            state["is_green"] = False
+                        keypoint_absence_timer_start = None
+            else:
+                keypoint_absence_timer_start = None  # No green borders, no need to reset
+
             overlay = frame.copy()
             alpha = 0.5
             for idx, border in enumerate(scaled_borders_pts):
-                if border_states[idx]:
-                    color = (0, 255, 0)
+                if border_states[idx]["is_green"]:
+                    color = (0, 255, 0)  # Green
                 else:
-                    color = (0, 255, 255)
+                    color = (0, 255, 255)  # Yellow
                 cv2.fillPoly(overlay, [border], color)
             cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
-
-            border_states = [False] * len(borders)
 
             frame_show = cv2.resize(frame, (1280, 720))
             cv2.imshow("THREADPOOL EXECUTOR - Pose Detection with Borders", frame_show)
@@ -157,7 +226,7 @@ def process_frames(frame_queue):
 
 def main():
     global stop_flag, model
-    cap = cv2.VideoCapture("rtsp://admin:oracle2015@192.168.100.65:554/Streaming/Channels/1")
+    cap = cv2.VideoCapture("rtsp://admin:oracle2015@10.5.0.170:554/Streaming/Channels/1")
     if not cap.isOpened():
         print("Gagal membuka stream video. Periksa URL atau koneksi.")
         return
