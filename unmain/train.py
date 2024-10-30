4549450c8922153c24f080f84af15348e12780cf
Troppo99 <troppolungo99@gmail.com>
2024-10-30 14:33:39 +0700

    update train

--- unmain/train.py
+++ unmain/train.py
@@ -4,13 +4,13 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 from ultralytics import YOLO
 
 if __name__ == "__main__":
-    model = YOLO("yolo11l-pose.pt")
+    model = YOLO("yolo11l.pt")
     model.train(
-        data="D:/SBHNL/Images/AHMDL/ALKBR/BROOM_V5/data.yaml",
+        data="D:/NWR/datasets/BROOM_DETECT.v1i.yolov11/data.yaml",
         epochs=100,
         imgsz=640,
-        project="run/broom5_yolo11",
-        name="version1",
+        project="run/broom",
+        name="version_detect",
         device="cuda",
         batch=16,
         resume=False,
