1c181f32ab92de8f5ee7b018c90c09997d33084b
Troppo99 <troppolungo99@gmail.com>
2024-12-04 10:11:46 +0700

    update

--- assets/train.py
+++ assets/train.py
@@ -6,11 +6,11 @@ from ultralytics import YOLO
 if __name__ == "__main__":
     model = YOLO("models/yolo11l.pt")
     model.train(
-        data="D:/NWR/datasets/Paper_Two/data.yaml",
+        data="D:/NWR/datasets/Paper_Three/data.yaml",
         epochs=100,
         imgsz=640,
         project="run/paper",
-        name="ONE&TWO",
+        name="THREE",
         device="cuda",
         batch=16,
         resume=False,
