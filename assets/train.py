833311c84414588678cc288e9812e6517ad60543
Troppo99 <troppolungo99@gmail.com>
2024-12-06 14:00:56 +0700

    update

--- assets/train.py
+++ assets/train.py
@@ -6,11 +6,11 @@ from ultralytics import YOLO
 if __name__ == "__main__":
     model = YOLO("models/yolo11l.pt")
     model.train(
-        data="D:/NWR/datasets/Paper_Three/data.yaml",
+        data="D:/NWR/datasets/cutting_engine/data.yaml",
         epochs=100,
         imgsz=640,
-        project="run/paper",
-        name="THREE",
+        project="run/cutting_engine",
+        name="version1",
         device="cuda",
         batch=16,
         resume=False,
